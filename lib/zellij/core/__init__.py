82a517a5a26daa49403315dc1a6ce0e1027064d4
thomas.firmin19@gmail.com <thomas.firmin19@gmail.com>
2022-12-23 00:58:42 +0100

    Add shortcuts to loss_func.py.

--- lib/zellij/core/__init__.py
+++ lib/zellij/core/__init__.py
@@ -5,3 +5,12 @@
 # @Last modified by:   tfirmin
 # @Last modified time: 2022-10-03T22:36:18+02:00
 # @License: CeCILL-C (http://www.cecill.info/index.fr.html)
+
+from .loss_func import Loss, MockModel
+from .search_space import (
+    MixedSearchspace,
+    ContinuousSearchspace,
+    DiscreteSearchspace,
+)
+from .variables import IntVar, FloatVar, CatVar, ArrayVar, Constant, Variable
+from .objective import Minimizer, Maximizer, Lambda
