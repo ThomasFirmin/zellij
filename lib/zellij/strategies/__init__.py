0cee47fa491a715ac66c252e625544f18aebec27
thomas.firmin19@gmail.com <thomas.firmin19@gmail.com>
2022-12-23 01:05:46 +0100

    Shortcuts for all strategies

--- lib/zellij/strategies/__init__.py
+++ lib/zellij/strategies/__init__.py
@@ -5,3 +5,12 @@
 # @Last modified by:   tfirmin
 # @Last modified time: 2022-10-03T22:39:10+02:00
 # @License: CeCILL-C (http://www.cecill.info/index.fr.html)
+
+from .bayesian_optimization import Bayesian_optimization
+from .chaos_algorithm import CGS, CLS, CFS, Chaotic_optimization
+from .dba import DBA
+from .genetic_algorithm import Genetic_algorithm
+from .ils import ILS, ILS_section
+from .phs import PHS
+from .sampling import Center, Diagonal, Chaos, Chaos_Hypersphere
+from .simulated_annealing import Simulated_annealing
