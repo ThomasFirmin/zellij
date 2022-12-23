8e48298fe7681d5fa46998bcd98f55091e0a026c
thomas.firmin19@gmail.com <thomas.firmin19@gmail.com>
2022-12-23 01:11:23 +0100

    Utils shortcuts

--- lib/zellij/utils/__init__.py
+++ lib/zellij/utils/__init__.py
@@ -5,3 +5,29 @@
 # @Last modified by:   tfirmin
 # @Last modified time: 2022-10-03T22:38:40+02:00
 # @License: CeCILL-C (http://www.cecill.info/index.fr.html)
+
+from .converters import (
+    ArrayMinmax,
+    FloatMinmax,
+    IntMinmax,
+    CatMinmax,
+    ConstantMinmax,
+    ArrayBinning,
+    FloatBinning,
+    IntBinning,
+    CatBinning,
+    ConstantBinning,
+    Continuous,
+    Discrete,
+    DoNothing,
+)
+from .distances import Euclidean, Manhattan, Mixed
+from .neighborhoods import (
+    ArrayInterval,
+    FloatInterval,
+    IntInterval,
+    CatInterval,
+    ConstantInterval,
+    Intervals,
+)
+from .operators import NeighborMutation, DeapTournament, DeapOnePoint
