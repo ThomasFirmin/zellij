e501bf8e93885f59ba1d2162a82c075b4bb03ca0
thomas.firmin19@gmail.com <thomas.firmin19@gmail.com>
2024-02-14 15:22:40 +0100

    Update Chaos maps doc and minor changes

--- zellij/strategies/tools/chaos_map.py
+++ zellij/strategies/tools/chaos_map.py
@@ -1,61 +1,67 @@
-# @Author: Thomas Firmin <ThomasFirmin>
-# @Date:   2022-05-03T15:41:48+02:00
-# @Email:  thomas.firmin@univ-lille.fr
-# @Project: Zellij
-# @Last modified by:   tfirmin
-# @Last modified time: 2023-05-23T15:43:47+02:00
-# @License: CeCILL-C (http://www.cecill.info/index.fr.html)
+# Author Thomas Firmin
+# Email:  thomas.firmin@univ-lille.fr
+# Project: Zellij
+# License: CeCILL-C (http://www.cecill.info/index.fr.html)
 
-import contextlib
-import numpy as np
+from __future__ import annotations
 from abc import ABC, abstractmethod
+import contextlib
+from typing import Optional
+
+from zellij.core.errors import DimensionalityError
+
+import numpy as np
 
 import logging
 
 logger = logging.getLogger("zellij.chaos_map")
 
 
-class Chaos_map(ABC):
+class ChaosMap(ABC):
 
-    """Chaos_map
+    """ChaosMap
 
-    :code:`Chaos_map` is in abstract class describing what a chaos map is.
+    :code:`ChaosMap` is in abstract class describing what a chaos map is.
     It is used to sample solutions.
 
     Attributes
     ----------
-    vectors : int
-        Size of the map (rows).
+    nvectors : int
+        Map size (rows).
     params : int
         Number of parameters (columns).
-    seed : int
-        Seed for the random number generator.
     map : np.array
         Chaos map of shape (vectors, params).
+    seed
+        Seed of numpy RNG.
 
     See Also
     --------
-    Chaotic_optimization : Chaos map is used here.
+    ChaoticOptimization : Chaos map is used here.
     """
 
-    def __init__(self, vectors, params, seed):
-        self.vectors = vectors
+    def __init__(self, nvectors: int, params: int, seed=None):
+        self.nvectors = nvectors
         self.params = params
         self.seed = seed
 
-        self.map = np.zeros([self.vectors, self.params])
+        self.map = np.zeros([self.nvectors, self.params])
 
     @abstractmethod
-    def sample(self):
-        """sample()
+    def sample(self, seed: Optional[int] = None):
+        """sample
 
-        Create the chaotic map
+        Sample random chaotic map.
 
+        Parameters
+        ----------
+        seed : int
+            Seed for RNG.
         """
         pass
 
     @contextlib.contextmanager
-    def _temp_seed(self, seed):
+    def _temp_seed(self, seed: Optional[int]):
         state = np.random.get_state()
         np.random.seed(seed)
         try:
@@ -63,37 +69,38 @@ class Chaos_map(ABC):
         finally:
             np.random.set_state(state)
 
-    def __add__(self, other):
-        assert (
-            self.params == other.params
-        ), f"Error, maps must have equals `params`, got {self.params} and {other.params}"
+    def __add__(self, other: ChaosMap):
+        if self.params != other.params:
+            raise DimensionalityError(
+                f"Error, maps must have equals `params`, got {self.params} and {other.params}"
+            )
 
-        new_map = _AddedMap(
-            self.vectors + other.vectors, self.params, self, other, self.seed
-        )
+        new_map = _AddedMap(self.nvectors + other.nvectors, self.params, self, other)
 
         return new_map
 
 
-class _AddedMap(Chaos_map):
+class _AddedMap(ChaosMap):
     """_AddedMap
 
     Addition of two maps. Maps are shuffled.
 
     """
 
-    def __init__(self, vectors, params, map1, map2, seed):
-        self.vectors = vectors
-        self.params = params
-        self.seed = seed
+    def __init__(
+        self, nvectors: int, params: int, map1: ChaosMap, map2: ChaosMap, seed=None
+    ):
+        super().__init__(nvectors, params, seed)
+        self.map[: map1.nvectors, : map1.params] = map1.map
+        self.map[map2.nvectors :, map2.params :] = map2.map
 
-        self.map = np.append(map1.map, map2.map)
+    def sample(self, seed: Optional[int]):
+        # shuffles maps inplace
+        with self._temp_seed(seed if seed else self.seed):
+            np.random.shuffle(self.map)
 
-    def sample(self):
-        np.random.shuffle(self.map)
 
-
-class Henon(Chaos_map):
+class Henon(ChaosMap):
     """Henon chaotic map
 
     .. math::
@@ -107,7 +114,7 @@ class Henon(Chaos_map):
 
     Parameters
     ----------
-    vectors : int
+    nvectors : int
         Map size
     params : int
         Number of dimensions
@@ -115,30 +122,47 @@ class Henon(Chaos_map):
         Henon map parameter. Has an influence on the chaotic, intermittent or periodicity behaviors.
     b : float, default=0.305620406
         Henon map parameter. Has an influence on the chaotic, intermittent or periodicity behaviors.
-    seed : int
-        Seed for the random number generator.
 
     Attributes
     ----------
     map : numpy.ndarray
         Chaos map of size (vectors,param)
 
+    Examples
+    --------
+    >>> from zellij.strategies.tools import Henon
+    >>> cmap = Henon(5,2)
+    >>> print(cmap.map.shape)
+    (5, 2)
+    >>> print(cmap.map)
+    [[0.37113568 0.1903755 ]
+    [0.7771203  0.28780393]
+    [0.52583348 0.852618  ]
+    [1.         0.        ]
+    [0.         1.        ]]
     """
 
-    def __init__(self, vectors, params, a=1.4020560, b=0.305620406, seed=0):
-        super().__init__(vectors, params, seed)
+    def __init__(
+        self,
+        nvectors: int,
+        params: int,
+        a: float = 1.4020560,
+        b: float = 0.305620406,
+        seed=None,
+    ):
+        super().__init__(nvectors, params, seed)
 
         self.a = a
         self.b = b
-        self.sample(seed)
+        self.sample(self.seed)
 
-    def sample(self, seed):
-        with self._temp_seed(seed):
+    def sample(self, seed: Optional[int] = None):
+        with self._temp_seed(seed if seed else self.seed):
             # Initialization
-            y = np.zeros([self.vectors, self.params])
+            y = np.zeros([self.nvectors, self.params])
             x = np.random.random(self.params)
 
-            for i in range(1, self.vectors):
+            for i in range(1, self.nvectors):
                 # y_{k+1} = x_{k}
                 y[i, :] = self.b * x
 
@@ -154,7 +178,7 @@ class Henon(Chaos_map):
             self.map = (y - alpha) / (beta - alpha)
 
 
-class Kent(Chaos_map):
+class Kent(ChaosMap):
     """Kent chaotic map
 
     .. math::
@@ -170,34 +194,49 @@ class Kent(Chaos_map):
 
     Parameters
     ----------
-    vectors : int
-        Map size
+    nvectors : int
+        Map size.
     params : int
-        Number of dimensions
+        Number of dimensions.
     beta : float, default=0.8
         Kent map parameter. Has an influence on the chaotic, intermittent,
         convergence, or periodicity behaviors.
     seed : int
-        Seed for the random number generator.
+        Seed for the numpy RNG.
 
     Attributes
     ----------
     map : numpy.ndarray
         Chaos map of size (vectors,param)
 
+    Examples
+    --------
+    >>> from zellij.strategies.tools import Kent
+    >>> cmap = Kent(5,2)
+    >>> print(cmap.map.shape)
+    (5, 2)
+    >>> print(cmap.map)
+    [[0.27544269 0.90451904]
+    [0.34430336 0.47740478]
+    [0.4303792  0.59675598]
+    [0.53797399 0.74594498]
+    [0.67246749 0.93243122]]
+    
     """
 
-    def __init__(self, vectors, params, beta=0.8, seed=0):
-        super().__init__(vectors, params, seed)
+    def __init__(
+        self, nvectors: int, params: int, beta: float = 0.8, seed: Optional[int] = None
+    ):
+        super().__init__(nvectors, params, seed)
 
         self.beta = beta
-        self.sample(seed)
+        self.sample(self.seed)
 
-    def sample(self, seed):
-        with self._temp_seed(seed):
+    def sample(self, seed: Optional[int] = None):
+        with self._temp_seed(seed if seed else self.seed):
             self.map[0, :] = np.random.random(self.params)
 
-            for i in range(1, self.vectors):
+            for i in range(1, self.nvectors):
                 self.map[i, :] = np.where(
                     self.map[i - 1, :] < self.beta,
                     self.map[i - 1, :] / self.beta,
@@ -205,7 +244,7 @@ class Kent(Chaos_map):
                 )
 
 
-class Logistic(Chaos_map):
+class Logistic(ChaosMap):
     """Logistic chaotic map
 
     .. math::
@@ -218,37 +257,51 @@ class Logistic(Chaos_map):
     Parameters
     ----------
     vectors : int
-        Map size
+        Map size.
     params : int
-        Number of dimensions
+        Number of dimensions.
     mu : float, default=3.57
         Logistic map parameter. Has an influence on the chaotic, intermittent,
         convergence, or periodicity behaviors.
     seed : int
-        Seed for the random number generator.
+        Seed for the numpy RNG.
 
     Attributes
     ----------
     map : numpy.ndarray
         Chaos map of size (vectors,param)
 
+    Examples
+    --------
+    >>> from zellij.strategies.tools import Logistic
+    >>> cmap = Logistic(5,2)
+    >>> print(cmap.map.shape)
+    (5, 2)
+    >>> print(cmap.map)
+    [[0.91074398 0.97705604]
+    [0.2902031  0.08003061]
+    [0.73536739 0.26284379]
+    [0.69472983 0.69171224]
+    [0.75712665 0.7612897 ]]
+    
     """
 
-    def __init__(self, vectors, params, mu=3.57, seed=0):
-        super().__init__(vectors, params, seed)
-
+    def __init__(
+        self, nvectors: int, params: int, mu: float = 3.57, seed: Optional[int] = None
+    ):
+        super().__init__(nvectors, params, seed)
         self.mu = mu
-        self.sample(seed)
+        self.sample(self.seed)
 
-    def sample(self, seed):
-        with self._temp_seed(seed):
+    def sample(self, seed: Optional[int] = None):
+        with self._temp_seed(seed if seed else self.seed):
             self.map[0, :] = np.random.random(self.params)
 
-            for i in range(1, self.vectors):
+            for i in range(1, self.nvectors):
                 self.map[i, :] = self.mu * self.map[i - 1, :] * (1 - self.map[i - 1, :])
 
 
-class Tent(Chaos_map):
+class Tent(ChaosMap):
     """Tent chaotic map
 
     .. math::
@@ -279,18 +332,36 @@ class Tent(Chaos_map):
     map : numpy.ndarray
         Chaos map of size (vectors,param)
 
+    Examples
+    --------
+    >>> from zellij.strategies.tools import Logistic
+    >>> cmap = Logistic(5,2)
+    >>> print(cmap.map.shape)
+    (5, 2)
+    >>> print(cmap.map)
+    [[0.3254483  0.81079411]
+    [0.6508966  0.37841178]
+    [0.6982068  0.75682356]
+    [0.60358639 0.48635288]
+    [0.79282721 0.97270577]]
+    
     """
 
-    def __init__(self, vectors, params, mu=2 - 1e-10, seed=0):
-        super().__init__(vectors, params, seed)
-
+    def __init__(
+        self,
+        nvectors: int,
+        params: int,
+        mu: float = 1.9999999999,
+        seed: Optional[int] = None,
+    ):
+        super().__init__(nvectors, params, seed)
         self.mu = mu
+        self.sample(self.seed)
 
-    def sample(self, seed):
-        with self._temp_seed(seed):
+    def sample(self, seed: Optional[int] = None):
+        with self._temp_seed(seed if seed else self.seed):
             self.map[0, :] = np.random.random(self.params)
-
-            for i in range(1, self.vectors):
+            for i in range(1, self.nvectors):
                 self.map[i, :] = np.where(
                     self.map[i - 1, :] < 0.5,
                     self.mu * self.map[i - 1, :],
@@ -298,11 +369,11 @@ class Tent(Chaos_map):
                 )
 
 
-class Random(Chaos_map):
-    def __init__(self, vectors, params, seed=0):
-        super().__init__(vectors, params, seed)
+class Random(ChaosMap):
+    def __init__(self, nvectors: int, params: int, seed: Optional[int] = None):
+        super().__init__(nvectors, params, seed)
         self.sample(seed)
 
-    def sample(self, seed):
-        with self._temp_seed(seed):
-            self.map = np.random.random((self.vectors, self.params))
+    def sample(self, seed: Optional[int] = None):
+        with self._temp_seed(seed if seed else self.seed):
+            self.map = np.random.random((self.nvectors, self.params))
